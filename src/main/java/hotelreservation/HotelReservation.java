e2cf65c1738ec9d9ebcd36914e1925bc9f8cb9d8
bosskai <281980783@qq.com>
2014-11-18 22:31:25 +0800

    Revert "refactor"
    This reverts commit 8c974d0f26fd9b8cb841d48e20acd086bf2a014e.
    
    Conflicts:
    	src/test/java/hotelreservation/HotelReservationTest.java

--- src/main/java/hotelreservation/HotelReservation.java
+++ src/main/java/hotelreservation/HotelReservation.java
@@ -11,7 +11,7 @@ import java.util.*;
 public class HotelReservation {
     protected List<Hotel> hotelList;
 
-    public String bestChoice(final String date) throws Exception {
+    public HotelReservation() {
         hotelList = new ArrayList<Hotel>();
         Hotel lakeWood = new Hotel("LakeWood", 3, 110, 80, 90, 80);
         Hotel bridgeWood = new Hotel("BridgeWood", 4, 160, 110, 60, 50);
@@ -19,14 +19,15 @@ public class HotelReservation {
         hotelList.add(lakeWood);
         hotelList.add(bridgeWood);
         hotelList.add(ridgeWood);
+    }
 
-        String[] Array = date.split(":");
-        String customerType = "";
-        String needToFormatDate = null;
-        for (String dateString : Array) {
-            customerType = Array[0];
-            needToFormatDate = Array[1];
-        }
+    public String bestChoice(final String date) throws Exception {
+
+        String dateFormat = date.replace(" ", "");
+
+        String[] Array = dateFormat.split(":");
+        String customerType = customerType = Array[0];
+        String needToFormatDate = needToFormatDate = Array[1];
 
         final List<Date> listDate = parseDate(needToFormatDate);
 
@@ -65,30 +66,10 @@ public class HotelReservation {
     protected List<Date> parseDate(String date) throws ParseException {
         List<Date> listDate = new ArrayList<Date>();
         String[] dateArray = date.split(",");
-//        Date dateFormatAfter;
         DateFormat format = new SimpleDateFormat("ddMMMyyyy", Locale.ENGLISH);
         for (String dateString : dateArray) {
-//            dateFormatAfter = format.parse(stringFormat(dateString,"("));
-            listDate.add(format.parse(stringFormat(dateString, "(")));
+            listDate.add(format.parse(dateString.split("\\(")[0]));
         }
         return listDate;
     }
-
-
-    private String stringFormat(String original, String regex1) {
-        int separatorID = 0;
-        char[] chars = original.toCharArray();
-        int step = chars.length;
-        char splitter1 = regex1.toCharArray()[0];
-        for (int i = 0; i < step; i++) {
-            if (chars[i] == splitter1) {
-                separatorID = i;
-            }
-        }
-        String afterFormat = "";
-        afterFormat = original.substring(0, separatorID);
-        return afterFormat;
-
-    }
-
 }
--- src/test/java/hotelreservation/HotelReservationTest.java
+++ src/test/java/hotelreservation/HotelReservationTest.java
@@ -1,10 +1,5 @@
 package hotelreservation;
-
-import org.junit.Before;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
@@ -12,28 +7,27 @@ import static org.junit.Assert.assertTrue;
  * Created by bosskai on 2014/11/13.
  */
 public class HotelReservationTest {
-
-//    protected List<Hotel> hotelList;
+    protected HotelReservation hotelReservation = new HotelReservation();
 
     @Test
     public void should_answer_lakewood() throws Exception {
         String dateString = "Regular:16Mar2009(mon),17Mar2009(tues),18Mar2009(wed)";
         String expect = "LakeWood";
-        assertTrue(expect.equalsIgnoreCase(new HotelReservation().bestChoice(dateString)));
+        assertTrue(expect.equalsIgnoreCase(hotelReservation.bestChoice(dateString)));
     }
 
     @Test
     public void should_answer_bridgeWood() throws Exception {
         String dateString = "Regular:20Mar2009(fri),21Mar2009(sat),22Mar2009(sun)";
         String expect = "BridgeWood";
-        assertTrue(expect.equalsIgnoreCase(new HotelReservation().bestChoice(dateString)));
+        assertTrue(expect.equalsIgnoreCase(hotelReservation.bestChoice(dateString)));
     }
 
     @Test
     public void should_answer_ridgeWood() throws Exception {
         String dateString = "Rewards:26Mar2009(thur),27Mar2009(fri),28Mar2009(sat)";
         String expect = "RidgeWood";
-        assertTrue(expect.equalsIgnoreCase(new HotelReservation().bestChoice(dateString)));
+        assertTrue(expect.equalsIgnoreCase(hotelReservation.bestChoice(dateString)));
     }
 
 }
